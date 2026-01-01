af1636024977f51c67f7b91f99daf6e10a7caf79
Nic Dillon <dilloncnicholas@gmail.com>
2025-12-31 20:29:07 -0500

    init

--- client/src/hooks/use-auth.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { useAuth as useAuthFromProvider } from "@/components/providers/AuthProvider";
-
-export function useAuth() {
-  return useAuthFromProvider();
-}
